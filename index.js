beeab54de5624cbf309e7d3dadc7ecdd54ca75c9
vpraveen1136 <serenityscape0826@gmail.com>
2025-08-08 21:55:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ async function main() {
     console.log("🟡 Starting script...");
     const urlDateStr = getTodayDateString();
     //const url = `https://nsearchives.nseindia.com/products/content/sec_bhavdata_full_${urlDateStr}.csv`;
-    const url = "https://nsearchives.nseindia.com/products/content/sec_bhavdata_full_06082025.csv";
+    const url = "https://nsearchives.nseindia.com/products/content/sec_bhavdata_full_07082025.csv";
     console.log("📥 Downloading CSV:", url);
 
     const csvData = await downloadCSV(url);
